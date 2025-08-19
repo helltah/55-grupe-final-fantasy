bb07e4dee0429494ba334fd87b9303b7cb354125
Vitalij <203612403+helltah@users.noreply.github.com>
2025-08-19 12:46:05 +0300

    Header

--- client/src/templates/PublicLayout.jsx
+++ client/src/templates/PublicLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
+import { Header } from "../components/Header";
 
 export function PublicLayout() {
     return (
-         <>
-            <header>HEADER CONTENT</header>
+        <>
+            <Header />
             <Outlet />
-            <footer>FOOTER CONTENT</footer>
+            <footer>FOOTER COMPONENTS</footer>
         </>
     )
 }
+ 
