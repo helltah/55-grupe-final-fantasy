bd54232c76497309e401fd538101882109f3ec8d
Vitalij <203612403+helltah@users.noreply.github.com>
2025-08-20 10:12:10 +0300

    Not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { HomePage } from './pages/public/Home';
 import { PublicLayout } from './templates/PublicLayout';
+import { NotFound } from "./pages/public/NotFound";
 
 export function App() {
   return (
@@ -8,6 +9,7 @@ export function App() {
       <Routes>
         <Route element={<PublicLayout />}>
           <Route path='/' index element={<HomePage />} />
+          <Route path='*' index element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
