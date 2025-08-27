1330f66e80b51216b2390eca6b2d59653059b592
Vitalij <203612403+helltah@users.noreply.github.com>
2025-08-27 10:38:15 +0300

    Category form

--- client/src/pages/admin/categories/EditCategory.jsx
+++ client/src/pages/admin/categories/EditCategory.jsx
@@ -1,9 +1,17 @@
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
+import { AdminCategoryForm } from "../../../components/forms/AdminCategoryForm";
 
 export function AdminEditCategoryPage() {
     return (
         <main>
             <AdminPageTitle title="Edit categories" />       
+
+            <div className="container">
+                <div className="row">
+                    <AdminCategoryForm />
+                </div>
+            </div>
         </main>
+
     );
 }
