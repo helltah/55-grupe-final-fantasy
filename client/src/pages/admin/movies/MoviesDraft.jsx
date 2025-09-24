43d18bb5625bfee4139c901ac00eef862127c731
Vitalij <203612403+helltah@users.noreply.github.com>
2025-09-24 18:20:31 +0300

    admin movie table

--- client/src/pages/admin/movies/MoviesDraft.jsx
+++ client/src/pages/admin/movies/MoviesDraft.jsx
@@ -1,11 +1,14 @@
-import { AdminMoviesTable } from "../../../components/AdminMoviesTable";
+import { useContext } from "react";
+import { AdminMoviesTable } from "../../../components/admin=movies-table/AdminMoviesTable";
 import { AdminPageTitle } from "../../../components/AdminPageTitle";
+import { MoviesContext } from "../../../context/movies/MoviesContext";
 
 export function AdminMoviesDraftPage() {
+    const { adminMovies } = useContext(MoviesContext);
     return (
         <main>
             <AdminPageTitle title="Draft movie" />
-            <AdminMoviesTable />
+            <AdminMoviesTable movies={adminMovies.filter(m => m.status_name === 'draft')}/>
         </main>
     );
 }
